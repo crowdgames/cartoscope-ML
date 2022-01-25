531df2a942ca82795b53ca6d12a7f9711b3478a5
Kutub Gandhi <kksgandhi@gmail.com>
2022-01-25 10:18:14 -0500

    add function to test model

--- models.py
+++ models.py
@@ -137,3 +137,102 @@ def create_model(model_name, data_directory):
                            num_epochs=25)
 
     torch.save(model_ft.state_dict(), f"models/{model_name}.pt")
+
+def test_model(model_path, data_directory, project_name, model_info, output_csv_path=None):
+    ## Imports
+    from __future__ import print_function, division
+
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    from torch.optim import lr_scheduler
+    import numpy as np
+    import torchvision
+    from torchvision import datasets, models, transforms
+    import matplotlib.pyplot as plt
+    import time
+    import os
+    import copy
+    import sys
+    import pandas as pd
+    from pathlib import Path
+
+    plt.ion()   # interactive mode normalization for training
+
+    ## CONFIG
+    ## load model
+
+    device  = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model = models.resnet18(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+    model.load_state_dict(torch.load(model_path))
+    model = model.to(device)
+    model.eval()
+
+    ## Create custom class to maintain path names
+
+    class ImageFolderWithPaths(datasets.ImageFolder):
+        """Custom dataset that includes image file paths. Extends
+        torchvision.datasets.ImageFolder
+        """
+
+        # override the __getitem__ method. this is the method that dataloader calls
+        def __getitem__(self, index):
+            # this is what ImageFolder normally returns 
+            original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+            # the image file path
+            path = self.imgs[index][0]
+            # make a new tuple that includes original and the path
+            tuple_with_path = (original_tuple + (path,))
+            return tuple_with_path
+
+    ## setting up dataloaders
+
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    image_dataset = ImageFolderWithPaths(f"{data_directory}/test", transform)
+    dataloader    = torch.utils.data.DataLoader(image_dataset, batch_size=4, num_workers=4)
+    dataset_size  = len(image_dataset)
+    class_names   = image_dataset.classes
+
+    ## 
+
+    predicteds  = []
+    actuals     = []
+    img_name    = []
+    confidences = []
+
+    with torch.no_grad():
+        for inputs, labels, paths in dataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            # print(outputs)
+            vals, preds = torch.max(outputs, 1)
+            prediction_confidences = torch.functional.F.softmax(outputs, dim=-1)
+            selected_confidences = torch.max(prediction_confidences, 1)
+            for j in range(inputs.size()[0]):
+                predicteds.append(class_names[preds[j]])
+                actuals.append(class_names[labels[j]])
+                img_name.append(Path(paths[j]).stem)
+                confidences.append(selected_confidences.values[j].item())
+
+    ##
+
+    vals = pd.DataFrame({"predicted": predicteds, "actual": actuals, "image": img_name, "confidence": confidences})
+
+    vals["project"]    = project_name
+    vals["model_type"] = model_info
+
+    vals['same'] = vals['predicted'] == vals['actual']
+    if output_csv_path:
+        vals.to_csv(output_csv_path, index=False)
+    else:
+        print(vals)
+        print(vals.same.mean())
+    print("Done")
